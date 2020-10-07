c8bef8bd1c35bd3e2d3fbf010278caaee5ce815b
Your name <a.m.verdine@gmail.com>
2020-10-07 16:40:47 -0400

    Define action types in  action_types.js

---
+++ src/store/actions/action_types.js
@@ -0,0 +1,19 @@
+// will be boilerplate actions
+export const SUCCESS = "SUCCESS";
+
+export const FAILURE = "FAILURE";
+
+// updates authentication state of user
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+// saves profile data from Auth0 to global state
+export const ADD_PROFILE = "ADD_PROFILE";
+
+export const REMOVE_PROFILE = "REMOVE_PROFILE";
+
+// tracks changes and submits of user submitted text form
+export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
+
+export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
