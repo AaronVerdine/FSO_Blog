1ff5c528a85e11016fda06ec3506ea328ca7ecf9
Your name <a.m.verdine@gmail.com>
2020-10-14 00:20:01 -0400

    Add Callback to redirect after successful Auth0 login

---
+++ src/hooks/callback.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+// user is redirected HERE after logging in with Auth0, then redirected to authcheck then home page
+
+const Callback = (props) => <div>Callback</div>;
+
+export default Callback;
