c46ccff47284606810eb7e2ab11180814b1b9aa6
Your name <a.m.verdine@gmail.com>
2020-10-15 16:39:07 -0400

    Add pritve component to be used only by private routes and authenticated users

---
+++ src/hooks/privatecomponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// will be used in private routes nd only be accessible by authenticated users
+
+const PrivateComponent = (props) => {
+  return <div>Private Component</div>;
+};
+
+export default PrivateComponent;
