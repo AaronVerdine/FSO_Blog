41760f3cc26663b5e5ee57a00ba8aa211daaa617
Your name <a.m.verdine@gmail.com>
2020-10-16 17:41:20 -0400

    Update profile.js // Add useContext import

--- src/hooks/profile.js
+++ src/hooks/profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Context from "../utils/context";
 
 // display user profile data from authObj passed down through context
