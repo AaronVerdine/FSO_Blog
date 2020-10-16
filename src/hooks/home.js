fc173f7f04902b32fe46aa6457f4f556161c84e8
Your name <a.m.verdine@gmail.com>
2020-10-16 17:30:25 -0400

    Update home.js // add return statement in Home function component

--- src/hooks/home.js
+++ src/hooks/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = (props) => {
-  <div>Home</div>;
+const Home = () => {
+  return <div>Home</div>;
 };
 
-export default HTMLFormElement;
+export default Home;
