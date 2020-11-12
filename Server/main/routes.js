5f2aed81f0c44caa17584c9ce6bd2721a190866e
Your name <a.m.verdine@gmail.com>
2020-11-12 00:49:25 -0500

    Update /Server/routes.js w/ USER PROFILE INFO

--- Server/main/routes.js
+++ Server/main/routes.js
@@ -155,4 +155,81 @@ router.get("/api/get/allpostcomments", (req, res, next) => {
   );
 });
 
+/*
+    USER PROFILE SECTION
+*/
+
+router.post("/api/posts/userprofiletodb", (req, res, next) => {
+  const values = [
+    req.body.profile.nickname,
+    req.body.profile.email,
+    req.body.profile.email_verified,
+  ];
+
+  pool.query(
+    `INSERT INTO users(username, email, email_verified, date_created) 
+              VALUES ($1, $2, $3, NOW()) 
+              ON CONFLICT DO NOTHING`,
+    values,
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get("/api/get/userprofilefromdb", (req, res, next) => {
+  const email = req.query.email;
+  console.log(email);
+
+  pool.query(
+    `SELECT * FROM users
+              WHERE email=$1`,
+    [email],
+    (q_err, q_res) => {
+      res.json(q_err, q_res);
+    }
+  );
+});
+
+router.get("/api/get/userposts", (req, res, next) => {
+  const user_id = req.query.user_id;
+  console.log(user_id);
+
+  pool.query(
+    `SELECT * FROM posts 
+              WHERE user_id=$1`,
+    [user_id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get("/api/get/otheruserprofilefromdb", (req, res, next) => {
+  // const email = [ "%" + req.query.email + "%" ]
+  const username = String(req.query.username);
+
+  pool.query(
+    `SELECT * FROM users
+              WHERE username = $1`,
+    [username],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
+router.get("/api/get/otheruserposts", (req, res, next) => {
+  const username = String(req.query.username);
+
+  pool.query(
+    `SELECT * FROM posts
+              WHERE author = $1`,
+    [username],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+});
+
 module.exports = router;
