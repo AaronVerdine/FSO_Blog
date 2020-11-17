7b8f391ea99cdb03594169dc85f138dd1f02eb04
Your name <a.m.verdine@gmail.com>
2020-11-17 17:03:29 -0500

    Update authcheck.js to save user profile to DB

--- Client/src/utils/authcheck.js
+++ Client/src/utils/authcheck.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, Profiler } from "react";
 import history from "./history";
 import Context from "./context";
 import * as ACTIONS from "../store/actions/actions";
+
+import axios from "axios";
 
 // Utilize useEffect to update authentication state
 
@@ -11,15 +13,24 @@ const AuthCheck = () => {
   useEffect(() => {
     if (context.authObj.isAuthenticated()) {
       // Checkes if access tokens in local storage haven't expired and user is still logged in
+      const profile = context.authObj.userProfile;
       context.handleUserLogin();
-      context.handleUserAddProfile(context.authObj.userProfile);
-      history.replace("/");
+      context.handleUserAddProfile(profile);
+      axios
+        .post("/api/posts/userprofiletodb", profile)
+        .then(
+          axios.get("/api/get/userprofilefromdb", {
+            params: { email: Profiler.profile.email },
+          })
+        )
+        .then((res) => context.handleAddDBProfile(res.data))
+        .then(history.replace("/"));
     } else {
       context.handleUserLogout();
       context.handleUserRemoveProfile();
       history.replace("/");
     }
-  }, []);
+  }, [context.authObj.userProfile, context]);
 
   return <div></div>;
 };
