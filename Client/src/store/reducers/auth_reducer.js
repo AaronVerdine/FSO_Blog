d08f8830d52bbeed1ba541cec116424a871c62ee
Your name <a.m.verdine@gmail.com>
2020-11-17 18:53:51 -0500

    Update auth_reducer.js by adding SET/REMOVE profile actions

--- Client/src/store/reducers/auth_reducer.js
+++ Client/src/store/reducers/auth_reducer.js
@@ -4,10 +4,11 @@ import * as ACTION_TYPES from "../actions/action_types";
 
 export const initialState = {
   is_authenticated: false,
+  db_profile: null,
   profile: null,
 };
 
-export const AuthenReducer = (state = initialState, action) => {
+export const AuthReducer = (state = initialState, action) => {
   switch (action.type) {
     case ACTION_TYPES.LOGIN_SUCCESS:
       return {
@@ -28,6 +29,16 @@ export const AuthenReducer = (state = initialState, action) => {
       return {
         ...state,
         profile: null,
+      };
+    case ACTION_TYPES.SET_DB_PROFILE:
+      return {
+        ...state,
+        db_profile: action.payload,
+      };
+    case ACTION_TYPES.REMOVE_DB_PROFILE:
+      return {
+        ...state,
+        db_profile: null,
       };
     default:
       return state;
