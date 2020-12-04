047631234fbd0f4cd23aafab6b49f60f7507083f
Your name <a.m.verdine@gmail.com>
2020-12-04 17:07:39 -0500

    Update ShowPost component w/ post/allpostcomments GET requests

--- Client/src/components/showpost.js
+++ Client/src/components/showpost.js
@@ -43,8 +43,41 @@ const ShowPost = (props) => {
         }
     }, [stateLocal, props.location])                                                
     
-    
-    
+    useEffect(() => {
+        if(!props.location.state && !stateLocal.fetched) {
+
+            const post_id = props.location.pathname.substring(6)
+
+            axios.get('/api/get/post', {params: {post_id: post_id}})
+                 .then(res => res.data.length !== 0
+                        ? setState({...stateLocal, 
+                            fetched: true,
+                            likes: res.data[0].likes,
+                            like_user_ids: res.data[0].like_user_ids,
+                            post_title: res.data[0].title,
+                            post_body: res.data[0].body,
+                            post_author: res.data[0].author,
+                            post_id: res.data[0].pid
+                        })
+                        :null                 
+                    )
+                .catch((err) => console.log(err))
+        }
+    }, [stateLocal, props.location])
+
+    useEffect(() => {
+        if(!stateLocal.comments_arr) {
+            if(props.location.state) {
+                const post_id= props.location.pathname.substring(6)
+                
+                axios.get('/api/get/allpostcomments', {params: {post_id: post_id}})
+                     .then(res => res.data.length !== 0
+                                    ? setState({...stateLocal, comments_arr: [...res.data]})
+                                    : null)
+                    .catch((err) => console.log(err))
+            }
+        }
+    }, [props.location, stateLocal])
     
     
     return (  );
