74e576eebdd83e69cd862f2b1c56998a67447509
Your name <a.m.verdine@gmail.com>
2020-12-19 22:26:08 -0500

    Add package.json to root folder

---
+++ Client/build/precache-manifest.39defeae08f896fa91d2f7806b36ad1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416a69907334d089c8b460458fd81f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "354678595082a0315e70",
+    "url": "/static/js/2.145d1c6a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.145d1c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb868bd35d668678ea9c",
+    "url": "/static/js/main.fc852608.chunk.js"
+  },
+  {
+    "revision": "b9dfd277d95faf1a26fb",
+    "url": "/static/js/runtime-main.2c38cbf2.js"
+  }
+]);
